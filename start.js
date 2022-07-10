b1945cf34e6b789f30a6e0ddbd0d2ab6c9d3a448
Dean Gilewicz <gilewiczd@gmail.com>
2022-07-10 00:52:00 -0600

    [master] - update node version to 16

--- start.js
+++ start.js
@@ -1,29 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// Make sure we are running node 7.6+
-const [major, minor] = process.versions.node.split('.').map(parseFloat);
-if (major < 7 || (major === 7 && minor <= 5)) {
-  console.log('You are on an older version of node and need to upgrade. Please go to nodejs.org and download version 7.6 or greater.');
-  process.exit();
+// Make sure we are running node 16+
+const [major] = process.versions.node.split(".").map(parseFloat);
+if (major < 16) {
+	console.log(
+		"You are on an older version of node and need to upgrade. Please go to nodejs.org and download version 16 or greater."
+	);
+	process.exit();
 }
 
 // import environmental variables from our variables.env file
-require('dotenv').config({ path: 'variables.env' });
+require("dotenv").config({ path: "variables.env" });
 
 // Connect to our Database and handle any bad connections
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connection.on('error', (err) => {
-  console.error(`${err.message}`);
+mongoose.connection.on("error", (err) => {
+	console.error(`${err.message}`);
 });
 
 // Import all of our models
-require('./models/Place');
-require('./models/User');
+require("./models/Place");
+require("./models/User");
 
 // Start our app!
-const app = require('./app');
-app.set('port', process.env.PORT || 7777);
-const server = app.listen(app.get('port'), () => {
+const app = require("./app");
+app.set("port", process.env.PORT || 7777);
+const server = app.listen(app.get("port"), () => {
 	console.log(`Express running on PORT ${server.address().port}`);
 });
